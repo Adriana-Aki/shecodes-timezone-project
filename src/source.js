3492dffe3c9ac1647a4c9cf331e68d81fd55d850
Adriana-Aki <adriana.aki157@gmail.com>
2024-08-16 12:22:40 +0200

    adding cities

--- src/source.js
+++ src/source.js
@@ -1,10 +1,4 @@
 function setTime(event) {
-  let timeZoneLondon = "Europe/London";
-  let timeZoneMumbai = "Asia/Kolkata";
-  let timeZoneTokyo = "Asia/Tokyo";
-  let timeZoneNewYork = "America/New_York";
-  let timeZoneLosAngeles = "America/Los_Angeles";
-
   let selectedItem = event.target.value;
   console.log(selectedItem);
   if (selectedItem.length > 0) {
@@ -36,6 +30,8 @@ function setTime(event) {
 }
 
 function updateCity(timeZone, city) {
+  console.log(timeZone);
+  console.log(city);
   let dateCity = moment().tz(`${timeZone}`).format("DD MMMM YYYY");
   let timeCity = moment().tz(`${timeZone}`).format("hh:mm:ss");
   let dayPartCity = moment().tz(`${timeZone}`).format("A");
@@ -43,23 +39,65 @@ function updateCity(timeZone, city) {
   console.log(timeCity);
   console.log(timeCity);
 
-  let cityElement = document.querySelector(".city");
-  let dateElement = document.querySelector(".date");
-  let timeElement = document.querySelector(".time");
-  let dayPartElement = document.querySelector(".day-part");
+  let citiesElement = document.querySelector("#clock");
+  console.log(citiesElement);
+  citiesElement.innerHTML = `
+  <div class="cityclock">
+          <div class="city/date">
+            <div class="city">${city}</div>
+            <div class="date">${dateCity}</div>
+          </div>
+          <div class="current-time">
+            <span class="time">${timeCity}</span> <span class="day-part">${dayPartCity}</span>
+          </div>
+        </div>
+  <a href="/" class="home">All cities</a>
+  `;
+  console.log(citiesElement);
 
-  cityElement.innerHTML = city;
-  dateElement.innerHTML = dateCity;
-  timeElement.innerHTML = timeCity;
-  dayPartElement.innerHTML = dayPartCity;
+  // let cityElement = document.querySelector(".city");
+  // let dateElement = document.querySelector(".date");
+  //let timeElement = document.querySelector(".time");
+  //let dayPartElement = document.querySelector(".day-part");
+
+  //cityElement.innerHTML = city;
+  //dateElement.innerHTML = dateCity;
+  // timeElement.innerHTML = timeCity;
+  // dayPartElement.innerHTML = dayPartCity;
 }
+
+function updateDefaultCity() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesDayPartElement = losAngelesElement.querySelector(".day-part");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
+
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM	Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format("hh:mm:ss");
+    losAngelesDayPartElement.innerHTML = losAngelesTime.format("A");
+  }
+  // Paris
+  let parisElement = document.querySelector("#paris");
+  if (parisElement) {
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisDayPartElement = parisElement.querySelector(".day-part");
+    let parisTime = moment().tz("Europe/Paris");
+
+    parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
+    parisTimeElement.innerHTML = parisTime.format("hh:mm:ss");
+    parisDayPartElement.innerHTML = parisTime.format("A");
+  }
+}
+
+updateDefaultCity();
+setInterval(updateDefaultCity, 1000);
 
 let citySelect = document.querySelector("#cities");
 console.log(citySelect);
 citySelect.addEventListener("change", setTime);
 
-updateCity("Europe/London", "London");
-
-setInterval(function () {
-  updateCity("Europe/London", "London");
-}, 1000);
+let citiesElement = document.querySelector("#clock");
+console.log(citiesElement);
